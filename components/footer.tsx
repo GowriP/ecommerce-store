9ce403361eb18763fd1f0950d0d1b7a75b87df0f
Gowri <prakashgowri72@gmail.com>
2023-08-18 15:16:08 -0400

    Initial Commit

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-white border-t'>
+        <div className='mx-auto py-10'>
+            <div className='text-center text-s text-black'>
+                &copy; 2023 FakeStoreNameA, Inc. All rights reserved 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
